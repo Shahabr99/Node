6d8d0a3de957d274f7c9cdfe5ed3cc9974a52c66
root <root@Shahab>
2023-06-23 11:30:02 -0700

    step 1

---
+++ step1.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const process = require("process");
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.Log(err);
+      process.exit(1);
+    } else {
+      console.log("YESSSS!!!");
+    }
+  });
+}
+
+cat(process.argv(2));
